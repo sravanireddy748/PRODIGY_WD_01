a37651197e99089e1754df21b5cfecfdbb853f95
sravanireddy748 <kotlasravanireddy064@gmail.com>
2024-10-29 21:18:47 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
